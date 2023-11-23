59313a2f7e0577ce37e4783b68ce9847ede63d21
Ishan-Mulani <89037495+Ishan-Mulani@users.noreply.github.com>
2023-11-23 23:33:46 +0530

    day 11 die component
    Signed-off-by: Ishan-Mulani <89037495+Ishan-Mulani@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React, { Component } from "react";
+import RollDie from "./RollDie";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <RollDie />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Dice.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./Dice.css";
+
+class Dice extends Component {
+  static defaultProps = {
+    face: "1",
+  };
+  render() {
+    const { face } = this.props;
+    const numToText = {
+      1: "one",
+      2: "two",
+      3: "three",
+      4: "four",
+      5: "five",
+      6: "six",
+    };
+    return (
+      <div>
+        <i
+          className={`Dice-icon fa-solid fa-dice-${
+            numToText[`${face}`]
+          } fa-2xl`}
+        ></i>
+      </div>
+    );
+  }
+}
+
+export default Dice;
